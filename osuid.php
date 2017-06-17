c070788550882085e9689ea86a155cd8390d87f8
lslqtz <975296698@qq.com>
2017-06-17 14:46:36 +0800

    fake osu

--- osuid.php
+++ osuid.php
@@ -1,19 +1,34 @@
 <?php
 error_reporting(0);
-print("Enter Username:");
+function get_userid($username,$apikey) {
+	if ($apikey) {
+		$json=json_decode(file_get_contents("https://osu.ppy.sh/api/get_user?k=$apikey&u=$username"));
+		if (count($json)) {
+			$info['userid']=$json[0]->user_id;
+			$info['username']=$json[0]->username;
+			return $info;
+		}
+	}
+	stream_context_set_default(array('http'=>array('method'=>'HEAD')));
+	$info['userid']=str_replace('https://osu.ppy.sh/users/','',get_headers("https://osu.ppy.sh/users/$username",1)['Location']);
+	return $info;
+}
+echo "Enter Username:";
 $username=trim(fgets(STDIN));
-if ($username === '') { die("Please Enter Your Username.\n"); }
-$curl=curl_init();
-curl_setopt($curl,CURLOPT_URL,"https://osu.ppy.sh/users/$username");
-curl_setopt($curl,CURLOPT_HEADER,1);
-curl_setopt($curl,CURLOPT_NOBODY,1);
-curl_setopt($curl,CURLOPT_RETURNTRANSFER,1);
-curl_setopt($curl,CURLOPT_SSL_VERIFYPEER,0);
-curl_setopt($curl,CURLOPT_FOLLOWLOCATION,1);
-curl_exec($curl);
-$effective_url=curl_getinfo($curl,CURLINFO_EFFECTIVE_URL);
-curl_close($curl);
-$userid=str_replace('https://osu.ppy.sh/users/','',$effective_url);
-if (!is_numeric($userid)) { die("User Not Found!\n"); }
-echo "{$username}'s ID:{$userid}\n";
+if (empty($username)) {
+	die("Please Enter Your Username.\n");
+}
+if (isset(getopt('k')['k'])) {
+	echo "Enter APIKey:";
+	$apikey=trim(fgets(STDIN));
+	if (!$apikey) {
+		die("Please Enter Your APIKey.\n");
+	}
+}
+$info=get_userid($username,$apikey);
+if (!$info['userid']) {
+	die("User Not Found!\n");
+}
+$username=(!$info['username']) ? $username : $info['username'];
+echo "$username's ID:".$info['userid']."\n";
 ?>
